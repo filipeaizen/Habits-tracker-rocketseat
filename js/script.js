9cdfddd3b0e390013e239494e01a01d2f1fd6855
Filipe Aizen <filipe.mikael.pereira@gmail.com>
2023-01-20 20:49:33 -0300

    Adicionado responsividade e funcionalidade no checked

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 const form = document.querySelector('form');
 const nlwSetup = new NLWSetup(form)
+
+const data = {
+    run:['01-01','01-02','01-05'],
+    water:['01-01', '01-03','01-05'],
+    food:['01-02', '01-05','01-06'],
+}
+nlwSetup.setData(data)
+nlwSetup.load()
