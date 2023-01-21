710374d870d397e7be8f2eb4a9bedddbfb330085
Filipe Aizen <filipe.mikael.pereira@gmail.com>
2023-01-20 21:51:47 -0300

    Adicionado função para salvar dados no localstorage

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const form = document.querySelector('form');
 const nlwSetup = new NLWSetup(form);
 const button = document.querySelector('header button');
+
 button.addEventListener( 'click', add);
+form.addEventListener('change', save);
 
 function add(){
 const today = new Date().toLocaleDateString("pt-br").slice(0,-5);
@@ -14,10 +16,16 @@ if(dayExists){
 alert("Dia adicionado com sucesso 😎 " + today);
 nlwSetup.addDay(today);
 }
+
+function save(){
+    localStorage.setItem("NLWSetup@habits" , JSON.stringify(nlwSetup.data));
+}
 /* const data = {
     run:['01-01','01-02','01-05'],
     water:['01-01', '01-03','01-05'],
     food:['01-02', '01-05','01-06'],
 } */
-nlwSetup.setData(data)
-nlwSetup.load()
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits"));
+nlwSetup.setData(data);
+nlwSetup.load();
